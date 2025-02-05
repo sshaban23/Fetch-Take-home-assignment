60de77e57f9ee479acbbfd21db290c7559f6caa9
Sireen Shaban <sireen.shaban23@gmail.com>
2025-02-05 15:23:05 -0600

    Python code

---
+++ pythonCode.py
@@ -0,0 +1,9 @@
+#import libraries
+import pandas as pd
+import numpy as np
+
+#read csv files
+user_data = pd.read_csv('C:\\Users\\siree\\OneDrive\\Desktop\\take home assignment (fetch)\\USER_TAKEHOME.csv')
+transaction_data = pd.read_csv('C:\\Users\\siree\\OneDrive\\Desktop\\take home assignment (fetch)\\TRANSACTION_TAKEHOME.csv')
+products_data = pd.read_csv('C:\\Users\\siree\\OneDrive\\Desktop\\take home assignment (fetch)\\PRODUCTS_TAKEHOME.csv')
+
